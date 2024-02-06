eb6f9020110464be9c19830a7efa685de33726f6
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2024-02-06 10:59:21 +0100

    Created Timer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Timer from "./Timer";
+
 import "./App.css";
 
 export default function App() {
 	return (
 		<div className="App">
 			<h1>Pomodoro Timer App</h1>
+			<Timer />
 		</div>
 	);
 }
---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Timer.css";
+
+export default function Timer() {
+	return (
+		<div>
+			<h2>25:00</h2>
+			<button>Start</button>
+			<button>Pause</button>
+			<button>Reset</button>
+		</div>
+	);
+}
