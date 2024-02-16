469c4c63e1a64080c6001ece7a13dffef31d656c
Marie-Ange-L <76390117+Marie-Ange-L@users.noreply.github.com>
2024-02-16 14:18:46 +0100

    Added refresh background img functionality

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ const DEFAULT_SESSION_TYPE = "Focus";
 export default function App() {
 	const [sessionType, setSessionType] = useState(DEFAULT_SESSION_TYPE);
 	const [showModal, setShowModal] = useState(false);
+	const [backgroundUrl, setBackgroundUrl] = useState(
+		"https://source.unsplash.com/random/?free,landscape,nature"
+	);
 
 	const handleSessionChange = (type) => {
 		setSessionType(type);
@@ -34,8 +37,16 @@ export default function App() {
 		setShowModal(false);
 	};
 
+	const refreshBackgroundImage = () => {
+		const img = new Image();
+		img.src = `https://source.unsplash.com/random/?free,landscape,nature&_=${Date.now()}`;
+		img.onload = () => {
+			setBackgroundUrl(img.src);
+		};
+	};
+
 	return (
-		<div className="App">
+		<div className="App" style={{ backgroundImage: `url(${backgroundUrl})` }}>
 			<div className="container">
 				<SessionButtons handleSessionChange={handleSessionChange} />
 				<Timer sessionType={sessionType} />
@@ -47,9 +58,11 @@ export default function App() {
 				</div>
 				|
 				<div className="restore-default-settings">
-					<button onClick={restoreDefaultSettings}>
-						Restore default settings
-					</button>
+					<button onClick={restoreDefaultSettings}>Reset settings</button>
+				</div>
+				|
+				<div className="refresh-background">
+					<button onClick={refreshBackgroundImage}>Update Image</button>
 				</div>
 			</div>
 		</div>
